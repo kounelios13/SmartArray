ee35f91068d89de36f43218f035c0b8756fc7f14
kounelios13 <pkounelios@gmail.com>
2016-05-01 16:45:07 +0300

    added shuffle function

--- smartArray.js
+++ smartArray.js
@@ -45,4 +45,14 @@ function SmartArray(arr){
 	self.replaceArray=function(newArray){
 		self.array=newArray;
 	};
+	self.shuffle=function(){
+		var a=self.array;
+		var j, x, i;
+	    for (i = a.length; i; i -= 1) {
+	        j = Math.floor(Math.random() * i);
+	        x = a[i - 1];
+	        a[i - 1] = a[j];
+	        a[j] = x;
+	    }
+	};
 }
