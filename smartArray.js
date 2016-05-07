754464f68fb35a9b31dfe54e899afb205bdbe408
kounelios13 <pkounelios@gmail.com>
2016-05-07 23:18:46 +0300

    added tostring,sort functions

--- smartArray.js
+++ smartArray.js
@@ -19,32 +19,32 @@ function SmartArray(arr,byVal){
 		return String.fromCharCode(int(c));
 	}
 	var self=this;
-	self.array=arr||[];
-	if(self.array.constructor!=Array)
-		self.array=[];
+	var _array=arr||[];
+	if(_array.constructor!=Array)
+		_array=[];
 	if(byVal)
-		self.array=arr.slice() || [];
+		_array=arr.slice() || [];
 	
 	self.isEmpty=function(){
 		return self.length() == 0;
 	};
 	self.toChars=function(){
 		var chars=[];
-		for(var i=0,max=self.array.length;i<max;i++)
-			if(typeof self.array[i]=='number')
-				chars.push(toChar(self.array[i]));
+		for(var i=0,max=_array.length;i<max;i++)
+			if(typeof _array[i]=='number')
+				chars.push(toChar(_array[i]));
 		return chars;
 	};
 	self.frequency=function(item){
 		var counter=0;
-		var array=self.array;
+		var array=_array;
 		for(var i=0,max=array.length;i<max;i++)
 			if(array[i]==item)
 				counter++;
 		return counter;	
 	};
 	self.topItem=function(){
-		var array=self.array;
+		var array=_array;
 		var max_freq=self.frequency(array[0]);
 		var max_item=array[0];
 		for(var i=0,max=array.length;i<max;i++)
@@ -58,59 +58,68 @@ function SmartArray(arr,byVal){
 		console.log("Now every time you press a key its keycode will be saved in the array you specified");
 		if(isJqueryPresent)
 			$(window).keypress(function (e) {
-				self.array.push(e.which || e.keyCode);
+				_array.push(e.which || e.keyCode);
 			});
 		else
 			window.addEventListener("keypress",function(e){
-				self.array.push(e.which || e.keyCode);
+				_array.push(e.which || e.keyCode);
 			});
 		return self;
 	};
 	self.getArray=function(){
-		return self.array;
+		return _array;
 	};
 	self.getLastItem=function(){ 
-		var ar=self.array;
-		return !self.array?ar[ar.length-1]:null;
+		var ar=_array;
+		return !_array?ar[ar.length-1]:null;
 	};
 	self.replaceArray=function(newArray){
-		self.array=newArray;
+		_array=newArray;
 	};
 	self.shuffle=function(){
 		//Stack overflow question
 		//http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript
 		if(!self.isEmpty())
-			shuffle(self.array);
+			shuffle(_array);
 		else 
 			throw new Error("Array is empty");
 	};
 	self.pickRandomItem=function(){
 		//Shuffe a copy of the array(not the original array) and return the first element
-		return shuffle(self.array.slice())[0];
+		return shuffle(_array.slice())[0];
 	};
 	//Reimplementing Basic Array methods
 	self.length=function(){
-		return self.array.length;
+		return _array.length;
 	};
 	self.push=function(){
 		for(var i=0;i<arguments.length;i++)
-			self.array.push(arguments[i]);
+			_array.push(arguments[i]);
 	};
 	self.slice=function(start,stop){
-		return self.array.slice(start,stop);
+		return _array.slice(start,stop);
 	};
 	self.shift=function(){
-		return self.array.shift();
+		return _array.shift();
 	};
 	self.unshift=function(item){
 		if(item)
-			self.array.unshift(item);
-		return self.length();
+			return _array.unshift(item);
+		
 	};
 	self.reverse=function(){
-		return self.array.reverse();
+		return _array.reverse();
 	};
 	self.reduce=function(filter){
-		return self.array.reduce(filter);
+		return _array.reduce(filter);
+	};
+	self.sort=function(compareFunction){
+		return _array.sort(compareFunction);
+	}
+	self.toString=function(){
+		return _array.toString();
+	};
+	self.indexOf=function(item){
+		return _array.indexOf(item);
 	};
 }
