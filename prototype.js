938f4d25d8c6f66aef5be5e681d7a08121053808
kounelios13 <pkounelios@gmail.com>
2016-06-24 14:20:04 +0300

    more

--- prototype.js
+++ prototype.js
@@ -26,6 +26,9 @@ function log(o) {
 //Just for testing purposes
 var ar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c'];
 "use strict";
+Array.prototype.lastItem=function(){
+	return this[this.length-1];
+};
 Array.prototype.swap = function() {
 	var args = arguments;
 	var ar = this;
@@ -38,3 +41,64 @@ Array.prototype.swap = function() {
 	this[b] = this[a];
 	this[a] = temp;
 };
+Array.prototype.frequncy=function(item){
+	if(!item)
+		return 0;
+	var counter=0;
+	for(var i=0,max=this.length,ar=this;i<max;i++)
+		if(ar[i]==item)
+			counter++;
+	return counter;	
+};
+Array.prototype.uniqueItems=function(){
+	var ar=this,unique=[],max=ar.length;
+	for(var i = 0 ; i < max;i++)
+		if(unique.indexOf(ar[i] )==-1)
+			unique.push(ar[i]);
+	return unique;	
+};
+Array.prototype.removeDuplicates=function(){
+	var clear_array=this.uniqueItems();
+	this.length=0;
+	for(var i = 0;i<clear_array.length;i++)
+		this[i] = clear_array[i];
+	return this;
+};
+Array.prototype.shuffle=function(){
+	var a = this;
+	var j, x,i=a.length;
+		while(i){
+			j = Math.floor(Math.random() * i);
+		    x = a[i - 1];
+		    a[i - 1] = a[j];
+		    a[j] = x;
+		    i--;
+		    
+		}
+		return a;
+};
+Array.prototype.randomItem=function(){
+	return this.slice().shuffle()[0];
+};
+Array.prototype.topItem=function(){
+	var ar = this.slice().sort(function(a,b) {
+    	return ('number'=== typeof (a && b))?a-b:(a+"").localeCompare(b);
+    });
+	var max = ar.length;
+	var max_f = ar.frequncy(ar[0]);
+	var top = ar[0];
+	for(var i =  1 ;i < max; i++)
+		if(ar.frequncy(ar[i]) > max_f){
+			max=ar[i];
+			max_f=ar.frequncy(ar[i]);
+		}
+	return top;	
+};
+var x=[1,1,1,1,1,1,1,1,2,3,4,4,5,5,5,1,1,1];
+//x.removeDuplicates();
+//var y = x.shuffle();
+log(x.shuffle().topItem());
+t=[];
+for(i=0;i<1000000;i++)
+	t.push(x.topItem());
+log(t.length);
